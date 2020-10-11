d3ab97f4eb9481717a6ecc6d9661ee592638ba71
Adir Noyman <45500652+AdirNoyman@users.noreply.github.com>
2020-10-11 22:30:53 +0300

    Add files via upload

---
+++ WeSplitAdirApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAdirApp.swift
+//  WeSplitAdir
+//
+//  Created by אדיר נוימן on 10/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAdirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
